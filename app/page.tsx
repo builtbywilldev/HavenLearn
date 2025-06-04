e9a23c86a32ae692be8193dbfe6fbe24eddf5da5
builtbywilldev <builtbywill@proton.me>
2025-06-03 20:17:45 -0600

    finished the companion card for this mobile first tutor SaaS app!

--- app/page.tsx
+++ app/page.tsx
@@ -9,9 +9,30 @@ const Page = () => {
     <main>
       <h1 className="text-2xl underline">Top Companions</h1>
        <section className="home-section">
-        <CompanionCard />
-        <CompanionCard />
-        <CompanionCard />
+        <CompanionCard 
+          id="123"
+          name="Violet the Logic Hacker"
+          topic='Intro to offline Ai'
+          subject='Artificial Intelligence'
+          duration={45}
+          color='#ffda6e'
+        />
+        <CompanionCard 
+          id="456"
+          name="Harry the Number Wizard"
+          topic='Linear Algebra'
+          subject='Math'
+          duration={30}
+          color='#e5d0ff'
+        />
+        <CompanionCard 
+          id="789"
+          name="Victoria, the vocab builder"
+          topic='Language'
+          subject='English Literature'
+          duration={25}
+          color='#BDE7FF'
+        />
        </section>
 
         <section className="home-section">
