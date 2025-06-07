27d8f32e46a9861c9d5732b0897b2f58b516b7e5
builtbywilldev <builtbywill@proton.me>
2025-06-07 12:32:24 -0600

    ✅ Clerk fully integrated with Supabase auth + row-level security 🔗 Supabase now powers backend — companions insert logic live 👋 Appwrite deprecated — full send on Supabase stack -time to show the data in our App!

--- components/CompanionForm.tsx
+++ components/CompanionForm.tsx
@@ -22,6 +22,8 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { subjects } from "@/constants";
 import { Textarea } from "./ui/textarea";
+import { createCompanion } from "@/lib/actions/companion.actions";
+import { redirect } from "next/navigation";
 
 const formSchema = z.object({
   name: z.string().min(1, { message: "companion is required" }),
@@ -34,10 +36,16 @@ const formSchema = z.object({
 // 1. Define your form.
 
 // 2. Define a submit handler.
-const onSubmit = (values: z.infer<typeof formSchema>) => {
-  console.log(values);
-};
+const onSubmit = async (values: z.infer<typeof formSchema>) => {
+  const companion = await createCompanion(values)
 
+  if(companion){
+    redirect(`/companions/${companion.id}`);
+} else{
+  console.log('failed to create a cmompanion')
+  redirect('/')
+}
+}
 const CompanionForm = () => {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
